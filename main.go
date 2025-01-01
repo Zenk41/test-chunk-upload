d75d8d4eb8a180048698c86efe20c92a3b11c99a
Zenk41 <ardhidhani@gmail.com>
2025-01-01 23:19:07 +0700

    checkpoint be

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-import()
-
+type Response struct {
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
 
 func main(){
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/chunk-upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			r.FormFile("chunk")
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set( "Content-Type", "application/json")
 	
+			response := Response{
+				Status: "success",
+				Message: "this path has been accessed",
+			}
+	
+			if err := json.NewEncoder(w).Encode(response);err != nil {
+				http.Error(w, fmt.Sprintf("Error encoding JSON: %v", err), http.StatusInternalServerError)
+			}
+		} else if r.Method == "POST" {
+			
+		}
+	})
+
+
+	log.Printf("Server run on port 4000")
+	if err := http.ListenAndServe(":3000", mux); err != nil {
+		log.Fatalf("Cannot server the server on port :3000, err : %v ", err)
+	}
+ 
 }
